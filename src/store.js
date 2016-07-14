a7377b756cb6e60bceb26b194dec10112896a709
Pavel Vorona <pvorona@klika-tech.com>
2016-07-14 18:50:32 +0300

    add lodash to save to localStorage using throttle

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { todoReducer } from './todo/todos'
 import { newTodoReducer } from './todo/new-todo'
 import { multi } from './utils'
+import throttle from 'lodash/throttle'
 
 const initialState = {
   todos: [],
@@ -13,7 +14,12 @@ const reducers = combineReducers({
   newTodo: newTodoReducer
 })
 
-const store =
-  createStore(reducers, initialState, compose(applyMiddleware(multi), window.devToolsExtension && window.devToolsExtension()))
+const enhanser = compose(applyMiddleware(multi), window.devToolsExtension && window.devToolsExtension())
+
+const store = createStore(reducers, initialState, enhanser)
+
+store.subscribe(throttle(() => {
+  console.log('saving')
+}, 1000))
 
 export default store
