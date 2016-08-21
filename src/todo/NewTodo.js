f816db545ecbc6f3b1443501580f867302c49185
Pavel Vorona <pvorona@klika-tech.com>
2016-08-21 18:28:47 +0300

    clear new todo

--- src/todo/NewTodo.js
+++ src/todo/NewTodo.js
@@ -5,13 +5,21 @@ import { addTodo } from './todos/todo.creators'
 import { changeNewTodo, clearNewTodo } from './new-todo/new-todo.creators'
 import { getNewTodo } from './todo.selectors'
 
-const NewTodo = (props) =>
-  <TextInput
-    placeholder='What needs to be done?'
-    className='new-todo'
-    autoComplete="off"
-    {...props}
-  />
+const NewTodo = ({onKeyPress, onChange, clearNewTodo, value}) =>
+  <div style={{position: 'relative'}}>
+    <TextInput
+      placeholder='What needs to be done?'
+      className='new-todo'
+      autoComplete="off"
+      value={value}
+      onKeyPress={onKeyPress}
+      onChange={onChange}
+    />
+    <button
+      onClick={clearNewTodo}
+      className="c-input__clear"
+    />
+  </div>
 
 const mapDispatchToProps = (dispatch) => ({
   onKeyPress (e) {
@@ -21,6 +29,9 @@ const mapDispatchToProps = (dispatch) => ({
   },
   onChange (e) {
     dispatch(changeNewTodo({title: e.target.value}))
+  },
+  clearNewTodo () {
+    dispatch(clearNewTodo())
   }
 })
 
