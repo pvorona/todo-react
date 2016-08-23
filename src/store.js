d964b78263344fe6a7a1240821e9e56f0e34fc29
Pavel Vorona <pvorona@klika-tech.com>
2016-08-23 15:39:25 +0300

    dont use multi dispatch middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { todoReducer } from './todo/todos'
 import { newTodoReducer } from './todo/new-todo'
-import { multi } from './utils'
 import throttle from 'lodash/throttle'
 import { save, get } from './storage'
 
@@ -17,7 +16,8 @@ const reducers = combineReducers({
   newTodo: newTodoReducer
 })
 
-const enhancer = compose(applyMiddleware(multi), window.devToolsExtension && window.devToolsExtension())
+const enhancer = compose(window.devToolsExtension && window.devToolsExtension())
+// const enhancer = compose(applyMiddleware(multi), window.devToolsExtension && window.devToolsExtension())
 
 const store = createStore(reducers, initialState, enhancer)
 
