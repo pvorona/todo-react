7e938eeb3e0fe3b0932404f7efa4fb2c7e5e496a
Pavel Vorona <pvorona@klika-tech.com>
2016-09-17 00:02:51 +0300

    WHY DID YOU UPDATE

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import store from './store'
 
+// if (process.env.NODE_ENV !== 'production') {
+const {whyDidYouUpdate} = require('why-did-you-update')
+whyDidYouUpdate(React)
+// }
+
 ReactDOM.render(<App />, document.getElementById('root'))
